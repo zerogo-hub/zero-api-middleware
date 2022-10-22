cc264950fe4537fa6aeef057e446dae69cccf934
alex <alex_my@126.com>
2022-10-22 14:50:05 +0800

    ✨ feat(limiter): 修改注释

--- limiter/limiter.go
+++ limiter/limiter.go
@@ -16,7 +16,11 @@ var (
 // New 全局限流器，每隔 every 时间放入一个令牌，满 burst 个令牌后不放入新令牌
 func New(every time.Duration, burst int) zeroapi.Handler {
 
+	// 每隔 every 时间放入 1 个，初始放入 burst 个
 	limiter = rate.NewLimiter(rate.Every(every), burst)
+
+	// 每秒 10 个，上限 100
+	// limiter = rate.NewLimiter(rate.Limit(10), 100)
 
 	return func(ctx zeroapi.Context) {
 		if ctx.Method() == http.MethodOptions {
